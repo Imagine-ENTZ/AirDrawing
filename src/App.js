d2cc8713ba5b42decd46acc792ee2ea95fd0d649
rong5026 <rong5026@naver.com>
2023-01-10 23:55:36 +0900

    [UPDATE] 손 모델 버전 변경

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import './App.css';
-import React, { useRef, useEffect } from "react"
-import { Hands } from "@mediapipe/hands"
-import * as Handsmesh from "@mediapipe/hands"
-import * as cam from "@mediapipe/camera_utils"
-import Webcam from "react-webcam"
-import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils/drawing_utils"
+import React, { useRef, useEffect } from "react";
+import Webcam from "react-webcam";
+import { Hands, HAND_CONNECTIONS } from "@mediapipe/hands/hands";
+import {
+  drawConnectors,
+  drawLandmarks,
+} from "@mediapipe/drawing_utils/drawing_utils";
+import { Camera } from "@mediapipe/camera_utils/camera_utils";
+
 
 function App() {
-  const webcamRef = useRef(null)
-  const canvasRef = useRef(null)
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
 
-  function onResults(results) {
+  useEffect(() => {
+    const hands = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.3.1626903359/${file}`;
+      }
+    })
+    hands.setOptions({
+      maxNumHands: 1,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+    hands.onResults(onResults);
+
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      const camera = new Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          await hands.send({ image: webcamRef.current.video });
+        },
+        width: 1280,
+        height: 720,
+      });
+      camera.start();
+    }
+  }, []);
+
+  const onResults = (results) => {
     const videoWidth = webcamRef.current.video.videoWidth;
     const videoHeight = webcamRef.current.video.videoHeight;
     canvasRef.current.width = videoWidth;
     canvasRef.current.height = videoHeight;
     const canvasElement = canvasRef.current;
     const canvasCtx = canvasElement.getContext("2d");
-
     canvasCtx.save();
     canvasCtx.clearRect(0, 0, videoWidth, videoHeight);
     canvasCtx.translate(videoWidth, 0);
@@ -30,78 +60,52 @@ function App() {
       canvasElement.height
     );
     if (results.multiHandLandmarks) {
+      
       for (const landmarks of results.multiHandLandmarks) {
-        drawConnectors(canvasCtx, landmarks, Handsmesh.HAND_CONNECTIONS, {
-          color: '#00FF00',
-          lineWidth: 5
+        drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+          color: "#00FF00",
+          lineWidth: 5,
         });
-        drawLandmarks(canvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
+        drawLandmarks(canvasCtx, landmarks, { color: "#FFFFFF", lineWidth: 2 });
       }
     }
-    canvasCtx.restore()
+    canvasCtx.restore();
+  };
 
-  }
-
-  useEffect(() => {
-    const hands = new Hands({
-      locateFile: (file) => {
-        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-      }
-    })
-
-    hands.setOptions({
-      maxNumHands: 2,
-      modelComplexity: 1,
-      minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5
-    });
-
-    hands.onResults(onResults)
-
-    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null) {
-      const camera = new cam.Camera(webcamRef.current.video, {
-        onFrame: async () => {
-          await hands.send({ image: webcamRef.current.video })
-        },
-        width: 1280,
-        height: 720,
-      });
-      camera.start()
-    }
-  }, [])
   return (
     <div>
-      <h1>Hands Mesh</h1>
       <Webcam
         audio={false}
         mirrored={true}
         ref={webcamRef}
         style={{
           position: "absolute",
-          marginRight: "auto",
           marginLeft: "auto",
-          left: 0,
-          right: 0,
+          marginRight: "auto",
+          left: "0",
+          right: "0",
           textAlign: "center",
-          zIndex: 9,
+          zindex: 9,
           width: 800,
-          height: 600
-        }} />
+          height: 600,
+        }}
+      />
       <canvas
         ref={canvasRef}
         style={{
           position: "absolute",
-          marginRight: "auto",
           marginLeft: "auto",
-          left: 0,
-          right: 0,
+          marginRight: "auto",
+          left: "0",
+          right: "0",
           textAlign: "center",
-          zIndex: 9,
+          zindex: 9,
           width: 800,
-          height: 600
-        }} />
+          height: 600,
+        }}
+      ></canvas>
     </div>
-  )
+  );
 }
 
 export default App;
