7a9a106a122cc01c4339be85925438e6cb6f0751
rong5026 <rong5026@naver.com>
2023-01-11 17:27:52 +0900

    [ADD} 로그인 버튼누를시 main페이지로 이동

--- src/HomePage/Component/MainHome.jsx
+++ src/HomePage/Component/MainHome.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import "../HomePage.css"
 import { AwesomeButton } from 'react-awesome-button';
 import 'react-awesome-button/dist/styles.css';
+import {Link} from "react-router-dom";
 
 function MainHome() {
+
     return (
         <div className='MainHome'>
             <div className='MainHome_left'>
@@ -13,10 +15,16 @@ function MainHome() {
             <div className='MainHome_right'>
                 <div className='MainHome_right_title'>Study words while playing!!!</div>
                 <div className='main_login_button'>
+
+                    <Link to = {`/main`}>
                     <AwesomeButton
                         className='mainButton'
-                        type="secondary">Login
+                        type="secondary"
+                        >Login
+                        
                     </AwesomeButton>
+                    </Link>
+                    
                 </div>
 
                 <AwesomeButton
