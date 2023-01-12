add201be7d95559171a573a0dd1fcd214b59466e
rong5026 <rong5026@naver.com>
2023-01-12 15:14:16 +0900

    [ADD] 손가락 포지션

--- src/MainPage/Component/MediapipeHands.jsx
+++ src/MainPage/Component/MediapipeHands.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Webcam from "react-webcam";
 import { Hands, HAND_CONNECTIONS } from "@mediapipe/hands/hands";
 import {
@@ -6,10 +6,10 @@ import {
   drawLandmarks,
 } from "@mediapipe/drawing_utils/drawing_utils";
 import { Camera } from "@mediapipe/camera_utils/camera_utils";
-
+import "./MediapipeHands.css"
 
 function MediapipeHands() {
-  
+
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
@@ -25,12 +25,11 @@ function MediapipeHands() {
       minDetectionConfidence: 0.5,
       minTrackingConfidence: 0.5,
     });
+    
     hands.onResults(onResults);
+    
 
-    if (
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null
-    ) {
+    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null) {
       const camera = new Camera(webcamRef.current.video, {
         onFrame: async () => {
           await hands.send({ image: webcamRef.current.video });
@@ -39,20 +38,28 @@ function MediapipeHands() {
         height: 720,
       });
       camera.start();
-    }
+    }  
   }, []);
 
   const onResults = (results) => {
     const videoWidth = webcamRef.current.video.videoWidth;
     const videoHeight = webcamRef.current.video.videoHeight;
-    canvasRef.current.width = videoWidth;
-    canvasRef.current.height = videoHeight;
     const canvasElement = canvasRef.current;
     const canvasCtx = canvasElement.getContext("2d");
+    
+    canvasElement.width = videoWidth;
+    canvasElement.height = videoHeight;
+
+    //현재상태를 저장
     canvasCtx.save();
+    // 직사각형 영역의 픽셀을 투명한 검은색으로 설정
     canvasCtx.clearRect(0, 0, videoWidth, videoHeight);
+    // 비디오 가로만큼 이동해서 손그릴 캔버스를 웹캠과 일치하도록 설정
     canvasCtx.translate(videoWidth, 0);
+    // 뒤집기 
     canvasCtx.scale(-1, 1);
+
+    // 캔버스 이미지 그리기
     canvasCtx.drawImage(
       results.image,
       0,
@@ -60,22 +67,38 @@ function MediapipeHands() {
       canvasElement.width,
       canvasElement.height
     );
+
+    // 손가락 부분 그리기
     if (results.multiHandLandmarks) {
-      
+
       for (const landmarks of results.multiHandLandmarks) {
+        // 손가락 선
         drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
           color: "#00FF00",
-          lineWidth: 5,
+          lineWidth: 2,
         });
+        // 손가락 점
         drawLandmarks(canvasCtx, landmarks, { color: "#FFFFFF", lineWidth: 2 });
       }
+
+      // 손가락 포인트 값
+      // const x = results.multiHandLandmarks[0][0].x;
+      // const y = results.multiHandLandmarks[0][0].y;
+      // const z = results.multiHandLandmarks[0][0].z;
+      // console.log(x);
+      // console.log(y);
+      // console.log(z);
+      
     }
+    //save한 곳으로 이동
     canvasCtx.restore();
   };
 
+  
   return (
     <div>
       <Webcam
+        className="webcam"
         audio={false}
         mirrored={true}
         ref={webcamRef}
@@ -104,6 +127,8 @@ function MediapipeHands() {
           width: 800,
           height: 600,
         }}
+
+       
       ></canvas>
     </div>
   );
