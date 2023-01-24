6457ef26292016c5a27913fc2376129cca571be7
littleemm <lilysolar@naver.com>
2023-01-25 03:02:26 +0900

    [ADD] how to game 필요 파일만 추가

---
+++ src/gamePage/HowToDecorative.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./DecorativeGameLobby.css";
+import StarRain from "./component/StarRain.jsx";
+import Direction from "./img/direction-arrow.png";
+import { useNavigate } from "react-router-dom";
+
+function DecorativeGameLobby() {
+    const navigate = useNavigate();
+
+
+    return (
+        <div className="main-container-how-decorative">
+            
+        </div>
+
+    );
+};
+
+export default DecorativeGameLobby;
