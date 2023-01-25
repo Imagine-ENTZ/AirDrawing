ed4462d288025942ff0c24f679f7f0cc6ec646aa
littleemm <lilysolar@naver.com>
2023-01-25 14:48:47 +0900

    [ADD] 레이아웃 설정

--- src/game/HowToDecorative.jsx
+++ src/game/HowToDecorative.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./DecorativeGameLobby.css";
+import "./HowToDecorative.css";
 import StarRain from "./component/StarRain.jsx";
 import Direction from "./img/direction-arrow.png";
 import { useNavigate } from "react-router-dom";
@@ -10,7 +10,15 @@ function DecorativeGameLobby() {
 
     return (
         <div className="main-container-how-decorative">
-            
+            <div className="top-container-how-decorative">
+
+            </div>
+            <div className="body-container-how-decorative">
+
+            </div>
+            <div className="bottom-container-how-decorative">
+
+            </div>
         </div>
     );
 };
