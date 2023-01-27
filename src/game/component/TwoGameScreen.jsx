a4ef03cae7e192d689cd78c4f49b5a7fc55ec04b
rong5026 <rong5026@naver.com>
2023-01-27 16:15:26 +0900

    [UPDATE] stun turn서버 구축

--- src/game/component/TwoGameScreen.jsx
+++ src/game/component/TwoGameScreen.jsx
@@ -705,16 +705,28 @@ const TwoGameScreen = forwardRef((props, ref) => {
     async function makeConnection() {
         myPeerConnection = new RTCPeerConnection({
             iceServers: [
+                
+                    // {
+                //     urls: [
+                //         'stun:stun.l.google.com:19302',
+                //         'stun:stun1.l.google.com:19302',
+                //         'stun:stun2.l.google.com:19302',
+                //         'stun:stun3.l.google.com:19302',
+                //         'stun:stun4.l.google.com:19302',
+                //     ],
+                // },
+
                 {
-                    urls: [
-                        'stun:stun.l.google.com:19302',
-                        'stun:stun1.l.google.com:19302',
-                        'stun:stun2.l.google.com:19302',
-                        'stun:stun3.l.google.com:19302',
-                        'stun:stun4.l.google.com:19302',
-                       
-                    ],
+                    urls: ['stun:stun.hongyeonghwan.com:3478'],
+                    username : "guest",
+                    credential: "somepassword",
                 },
+                {
+                    urls: ['turn:turn.hongyeonghwan.com:3478'],
+                    username : "guest",
+                    credential: "somepassword",
+                }
+                
             ],
         });
 
