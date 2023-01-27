2c096ce5938ccbb2cfe1ed8f1530bf965084bb38
littleemm <lilysolar@naver.com>
2023-01-27 19:02:09 +0900

    [UPDATE] token 업데이트

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -35,7 +35,7 @@ export const REGISTER_URL = `https://entz.azurewebsites.net/member`//`http://loc
 export const GAMEROOM_URL = `https://entz.azurewebsites.net/room`//`http://localhost:8080/room/`
 export const SOCKET_JS = `https://entz.azurewebsites.net/play`;  //`http://localhost:8080/play`;
 
-export const AUTHORIZATION_IMAGE = 'Bearer eyJ0eXAiOiJhdCtKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtbnpjTTZta2Z3NFM5UnB6RGc2UTVkWlVpS0FRYXp0SVd2MUFZYnNwc0JRZTNtTkkiLCJpc3MiOiJodHRwczpcL1wvYXBpLmZsYXRpY29uLmNvbVwvb2F1dGhcL3Rva2VuIiwiZXhwIjoxNjc0ODk2OTEyLCJpYXQiOjE2NzQ4MTA1MTIsImp0aSI6ImJhMjg2NzMzLThlZGItNDMwOS1iYzgwLTE1MDQ3OTg1M2YzOSIsImNsaWVudF9pZCI6Im1uemNNNm1rZnc0UzlScHpEZzZRNWRaVWlLQVFhenRJV3YxQVlic3BzQlFlM21OSSJ9.B70Pu7b2JP5AxuB4Kb8pgs5d9WDL2B8KlOtn7K6dmSQ';
+export const AUTHORIZATION_IMAGE = 'Bearer eyJ0eXAiOiJhdCtKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtbnpjTTZta2Z3NFM5UnB6RGc2UTVkWlVpS0FRYXp0SVd2MUFZYnNwc0JRZTNtTkkiLCJpc3MiOiJodHRwczpcL1wvYXBpLmZsYXRpY29uLmNvbVwvb2F1dGhcL3Rva2VuIiwiZXhwIjoxNjc0OTAwMDgxLCJpYXQiOjE2NzQ4MTM2ODEsImp0aSI6IjM4Nzg4MDU2LTljZDctNGM0NC1iZjJkLTgzYzM1YmM5NDk4OSIsImNsaWVudF9pZCI6Im1uemNNNm1rZnc0UzlScHpEZzZRNWRaVWlLQVFhenRJV3YxQVlic3BzQlFlM21OSSJ9.IU-yGk7PKMLEs0W20H_GeGEtYjtkJoobAUNrDFZ_yTI';
 
 export const DECORATIVE_GAME = 2;
 export const WORDTRACING_GAME = 1;
